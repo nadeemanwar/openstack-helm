73958db137dd8fbe59d3a6a4457ba93b4592324e
nadeem <na1431@att.com>
2017-02-15 17:01:41 -0500

    Added logic to print the log only if there is error Added the debug statement and fix the exit status if grep

--- travis-ci/charts_dry_run.sh
+++ travis-ci/charts_dry_run.sh
@@ -2,5 +2,13 @@
 
 for chart in *.tgz; do
   echo "Running helm install --dry-run --debug on $chart";
-  helm install --dry-run --debug local/$chart;
+  helm install --dry-run --debug local/$chart 2>&1 > /tmp/dry-run-output.log
+  if [ $? -ne 0 ];
+  then
+    echo "Found error printing the log"
+    cat /tmp/dry-run-output.log
+  else
+    echo "No Error found for dry run checking for gotpl"
+    grep gotpl /tmp/dry-run-output.log || true
+  fi
 done
