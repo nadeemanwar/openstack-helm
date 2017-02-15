0024e9b929104230cd271f139d8ddd0a505a0e0c
nadeem <na1431@att.com>
2017-02-15 17:01:41 -0500

    Added logic to print the log only if there is error

--- travis-ci/charts_dry_run.sh
+++ travis-ci/charts_dry_run.sh
@@ -2,5 +2,11 @@
 
 for chart in *.tgz; do
   echo "Running helm install --dry-run --debug on $chart";
-  helm install --dry-run --debug local/$chart;
+  helm install --dry-run --debug local/$chart 2>&1 > /tmp/dry-run-output.log
+  if [ $? -ne 0 ];
+  then
+    cat /tmp/dry-run-output.log
+  else
+    grep gotpl /tmp/dry-run-output.log
+  fi
 done
