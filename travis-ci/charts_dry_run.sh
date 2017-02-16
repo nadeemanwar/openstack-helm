fc8f67d117647af412247059c1d25a2fbd69a23d
nadeem <na1431@att.com>
2017-02-16 09:29:40 -0500

    Added the logic to fail the build if gotpl found in the log file

--- travis-ci/charts_dry_run.sh
+++ travis-ci/charts_dry_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+status=0;
 for chart in *.tgz; do
   echo "Running helm install --dry-run --debug on $chart";
   helm install --dry-run --debug local/$chart 2>&1 > /tmp/dry-run-output.log
@@ -7,8 +8,15 @@ for chart in *.tgz; do
   then
     echo "Found error printing the log"
     cat /tmp/dry-run-output.log
+    status=$?;
   else
     echo "No Error found for dry run checking for gotpl"
-    grep gotpl /tmp/dry-run-output.log || true
-  fi
+    if [ `grep gotpl /tmp/dry-run-output.log | wc -l` -ne 0 ]; 
+    then
+      echo ("Found gotpl, setting the status to 1 and printing the log") 
+      status=1;
+      cat /tmp/dry-run-output.log 
+    fi
+ fi
 done
+exit $status
