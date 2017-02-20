92ddc908a9aca0fafc66dcf748d99639fd509113
nadeem <na1431@att.com>
2017-02-20 15:58:32 -0500

    Added the logic for printing only error log at the end

--- travis-ci/charts_dry_run.sh
+++ travis-ci/charts_dry_run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 status=0;
+echo "" > /tmp/dry-run-errors.log
 for chart in *.tgz; do
   echo "Running helm install --dry-run --debug on $chart";
   helm install --dry-run --debug local/$chart 2>&1 > /tmp/dry-run-output.log
   if [ $? -ne 0 ];
   then
     echo "Found error printing the log"
-    cat /tmp/dry-run-output.log
-    status=$?;
+    cat /tmp/dry-run-output.log >> /tmp/dry-run-errors.log
+    status=1;
   else
     echo "No Error found for dry run checking for gotpl"
-    if [ `grep gotpl /tmp/dry-run-output.log | wc -l` -ne 0 ]; 
+    if [ `egrep -f travis-ci/errors.list /tmp/dry-run-output.log | wc -l` -ne 0 ]; 
     then
-      echo "Found gotpl, setting the status to 1 and printing the log" 
+      echo "Found errors, setting the status to 1 and printing the log" 
       status=1;
-      cat /tmp/dry-run-output.log 
+      cat /tmp/dry-run-output.log >> /tmp/dry-run-errors.log 
     fi
  fi
 done
