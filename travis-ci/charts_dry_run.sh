2ba3d4185acc8b3b84a31869baaaa1cedeb321ab
nadeem <na1431@att.com>
2017-02-22 09:24:59 -0500

    Added code to print the successful and failed charts

--- travis-ci/charts_dry_run.sh
+++ travis-ci/charts_dry_run.sh
@@ -15,9 +15,15 @@
 # limitations under the License.
 
 status=0
+total_charts=0
+failed_charts=0
+charts_with_warning=0
+successful_charts=0
+
 echo "" > /tmp/dry-run-errors.log
 echo "" > /tmp/dry-run-warnings.log
 for chart in *.tgz; do
+  (( total_charts++ ))
   echo "Running helm install --dry-run --debug on $chart";
   helm install --dry-run --debug local/$chart 2>&1 > /tmp/dry-run-output.log
   if [ $? -ne 0 ];
@@ -25,24 +31,38 @@ for chart in *.tgz; do
     echo "Found error and setting status to 1"
     cat /tmp/dry-run-output.log >> /tmp/dry-run-errors.log
     status=1;
+    (( failed_charts++ )) 
   else
-    echo "No Error found for dry run checking for these error phrases"
-    cat travis-ci/errors.list
+    echo "Install Successful, checking for template issues"
+    #cat travis-ci/errors.list
     if [ `egrep -f travis-ci/errors.list /tmp/dry-run-output.log | wc -l` -ne 0 ]; 
     then
       echo "Found errors, setting the status to 1 and printing the log" 
       status=1;
-      egrep -f travis-ci/errors.list /tmp/dry-run-output.log
-      cat /tmp/dry-run-output.log >> /tmp/dry-run-errors.log
+      (( failed_charts++ )) 
+      egrep -f travis-ci/errors.list /tmp/dry-run-output.log >> /tmp/dry-run-errors.log
+     # cat /tmp/dry-run-output.log >> /tmp/dry-run-errors.log
+    else
+       (( successful_charts++ )) 
     fi
-    echo "No Error found for dry run checking for for these warning phrases"
-    cat travis-ci/warnings.list
+    echo "Checking warning issues with the templates"
+    #cat travis-ci/warnings.list
     if [ `egrep -f travis-ci/warnings.list /tmp/dry-run-output.log | wc -l` -ne 0 ]; 
     then
-        echo "Found warnings" 
-        egrep -f travis-ci/warnings.list /tmp/dry-run-output.log
-        cat /tmp/dry-run-output.log >> /tmp/dry-run-warnings.log
+        echo "Found warnings"
+        (( charts_with_warning++ )) 
+        egrep -f travis-ci/warnings.list /tmp/dry-run-output.log >> /tmp/dry-run-warnings.log
+        #cat /tmp/dry-run-output.log >> /tmp/dry-run-warnings.log
     fi
  fi
 done
+echo "==============================================================="
+echo ""
+echo "Total charts = $total_charts"
+echo "Successful chart Install = $successful_charts"
+echo "Failed Charts Install = $failed_charts"
+echo "Charts Installed with warnings = $charts_with_warning"
+echo ""
+echo "==============================================================="
+
 exit $status
