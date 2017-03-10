91f7f1fbcb8d2e88f1a72fbbe2c77c00b7f4da44
Nadeem <nadeem@att.com>
2017-03-10 14:59:17 -0500

    fix start of container

--- circle-ci/kubeadm_setup.sh
+++ circle-ci/kubeadm_setup.sh
@@ -16,6 +16,7 @@
 
 docker run -it -e quay.io/attcomdev/kubeadm-ci:v1.1.0 --name kubeadm-ci --privileged=true -d --net=host --cap-add=SYS_ADMIN -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v /var/run/docker.sock:/var/run/docker.sock quay.io/attcomdev/kubeadm-ci:v1.1.0 /sbin/init
 
+docker ps -a
 sudo lxc-info --name kubeadm-ci
 sudo lxc-start --name kubeadm-ci
 sudo lxc-info --name kubeadm-ci
