7037cdc9dfb993c3ded5b6e57c80d5160acc557b
Nadeem <nadeem@att.com>
2017-03-23 13:47:33 -0400

    clean up

--- circle-ci/charts_dry_run.sh
+++ circle-ci/charts_dry_run.sh
@@ -17,20 +17,12 @@
 status=0
 for chart in *.tgz; do
   echo "Running helm install --dry-run --debug on $chart";
-  ${WORKDIR}/bin/helm install --dry-run --debug local/$chart 2>&1 > /tmp/dry-run-output.log
+  helm install --dry-run --debug local/$chart 2>&1 > /tmp/dry-run-output.log
   if [ $? -ne 0 ];
   then
     echo "Found error printing the log"
     cat /tmp/dry-run-output.log
     status=$?;
-  else
-    echo "No Error found for dry run checking for gotpl"
-    if [ `grep gotpl /tmp/dry-run-output.log | wc -l` -ne 0 ]; 
-    then
-      echo "Found gotpl, setting the status to 1 and printing the log" 
-      status=1;
-      cat /tmp/dry-run-output.log 
-    fi
  fi
 done
 exit $status
